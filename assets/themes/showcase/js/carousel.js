3d6af3a441e85a4217b265f689b9e66374e3cffb
twcmediakit <kyle.kinnaman@weather.com>
2013-11-14 12:59:10 -0500

    added carousel, fastclick

---
+++ assets/themes/showcase/js/carousel.js
@@ -0,0 +1,42 @@
+(function showcaseCarousel() {
+	var carousel_items = document.getElementsByClassName('carousel_item'),
+		carouselVideo = document.getElementsByClassName('carousel_video')[0],
+		nextVisible = 1,
+		runCarousel;
+
+	carousel_items[0].classList.add('visible');
+
+	function startCarousel() {
+		runCarousel = setInterval(function() {
+			moveCarousel(nextVisible);
+		}, 5000);
+	}
+
+	function moveCarousel(currentlyVisible) {
+		document.getElementsByClassName('visible')[0].classList.remove('visible');
+		nextVisible++;
+		carousel_items[currentlyVisible].classList.add('visible');
+		if(currentlyVisible === carousel_items.length - 1) {
+				nextVisible = 0;
+		}
+	}
+
+	if(carouselVideo) {
+		carouselVideo.addEventListener('play', function() {
+			stopCarousel();
+			carouselVideo.play();
+		});
+
+		carouselVideo.addEventListener('ended', function() {
+			moveCarousel(nextVisible);
+			startCarousel();
+			carouselVideo.load();
+		});
+	}
+
+	function stopCarousel() {
+		clearInterval(runCarousel);
+	}
+
+	startCarousel();
+}());
